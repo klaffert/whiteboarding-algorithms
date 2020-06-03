f6092b0630d1e7d4d21a08b17ff272d26c9e6427
Kelliann Lafferty <49892991+klaffert@users.noreply.github.com>
2020-06-02 18:55:32 -0700

    Submitted second working solution

--- reverseString.js
+++ reverseString.js
@@ -5,3 +5,14 @@ var reverseString = function(s) {
     let reversed = s.reverse()
   return reversed
 };
+
+// #2 Working Solution
+var reverseString = function(s) {
+  let left = 0
+  let right = s.length - 1
+  while (left < right) {
+    [s[left], s[right]]  = [s[right], s[left]]
+    left++
+    right--
+  }
+};
